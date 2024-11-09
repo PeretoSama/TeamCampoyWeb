a0869b328a6478ae419bf51cde9c47e3895ee84b
Pere <40438743+PeretoSama@users.noreply.github.com>
2024-11-09 03:48:41 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App () {
+  const obj = { test: 'te  st' }
+  console.log(obj.test)
+
+  return (
+    <>
+      <div>
+        TESTte
+      </div>
+    </>
+  )
+}
+
+export default App
